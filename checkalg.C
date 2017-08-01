beb7c88c6cf683c69262f50df7a3d024788b5a1e
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2017-08-01 18:33:33 +0200

    Add macro to verify Compression and split level

---
+++ checkalg.C
@@ -0,0 +1,32 @@
+//
+// Read a RAM file.
+//
+// Author: Fons Rademakers, 29/6/2017
+//
+
+#include <TFile.h>
+#include <TTree.h>
+#include <TRandom.h>
+#include <iostream>
+#include "ramrecord.h"
+#include <Compression.h>
+
+void checkalg(const char *file = "ramexample.root")
+{
+   auto f = TFile::Open(file);
+   auto t = (TTree*) f->Get("RAM");
+
+   RAMRecord *r = 0;
+
+   t->SetBranchAddress("RAMRecord.", &r);
+    
+   //t->Print();
+   std::cout << "LZMA = " << ROOT::kLZMA << std::endl;
+   std::cout << "GZIP = " << ROOT::kZLIB << std::endl;
+   std::cout << "COMPALG = " << f->GetCompressionAlgorithm() << std::endl;
+   std::cout << "COMPLEV = " <<  f->GetCompressionLevel() << std::endl;
+
+   TBranch *b = t->GetBranch("RAMRecord.");
+
+   std::cout << "SPLIT = " << b->GetSplitLevel() << std::endl;
+}
