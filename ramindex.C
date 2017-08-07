710076f2b2e56ae5bc5095106c8f0a3ff57274a2
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2017-08-07 13:35:31 +0200

    Define indexing script for RAM files

--- ramindex.C
+++ ramindex.C
@@ -1,76 +1,37 @@
 //
-// Build an index for a RAM file
+// Index for a RAM and save it to another file
 //
-// Author: Jose Javier Gonzalez Ortiz, 17/7/2017
+// Author: Jose Javier Gonzalez Ortiz, 7/8/2017
 //
 
-#include <fstream>
 #include <iostream>
-#include <string>
 
-#include <TFile.h>
 #include <TTree.h>
-#include <TStopwatch.h>
+#include <TTreeIndex.h>
+#include <TFile.h>
+#include <TString.h>
 
-#include "ramrecord.h"
+void ramindex(const char *file, const char *index_file, bool clone = true){
+    auto f = TFile::Open(file);
+    auto t = (TTree *)f->Get("RAM");
 
-void ramindex(const char *file)
-{
-   TStopwatch stopwatch;
-   stopwatch.Start();
+    t->BuildIndex("v_rname", "v_pos");
 
-   // Create index file
-   ofstream indexfile;
-   std::string indexfilename = file;
-   indexfilename += ".idx";
-   indexfile.open(indexfilename);
+    if(clone){
+        auto index_f = TFile::Open(index_file, "RECREATE");
+        index_f->SetCompressionLevel(f->GetCompressionLevel());
+        index_f->SetCompressionAlgorithm(f->GetCompressionAlgorithm());
+        TTree *indexed_t = t->CloneTree();
+        indexed_t->Print();
+        index_f->Write();
+    }
+    else{
+        auto i  = t->GetTreeIndex();
+        if(!i){
+            std::out << "Incorrect Index" << std::endl;
+            exit(1);
+        }
+    }
 
-   // Open the file and load tree and reader
-   auto f = TFile::Open(file);
-   auto t = (TTree *)f->Get("RAM");
-   RAMRecord *r = 0;
-
-   t->SetBranchAddress("RAMRecord.", &r);
-
-   // Assume RNAME are chunked together
-   // We look only at the RNAME column
-   t->SetBranchStatus("RAMRecord.*", 0);
-   t->SetBranchStatus("RAMRecord.v_rname", 1);
-   t->SetBranchStatus("RAMRecord.v_pos", 1);
-   
-   int i = 0;
-   t->GetEvent(0);
-   indexfile << "RNAME,begin,beginPOS,end,endPOS" << endl;
-   
-   indexfile << r->GetRNAME() << ",";
-   indexfile << i << ",";
-   indexfile << r->GetPOS() << ",";
-   
-   TString current_rname = r->GetRNAME();
-
-   for (i = 0; i < t->GetEntries(); i++) {
-      t->GetEvent(i);
-      if (!current_rname.EqualTo(r->GetRNAME())) {
-         current_rname = r->GetRNAME();
-
-         t->GetEvent(i-1);
-         indexfile << i-1 << ",";
-         indexfile << r->GetPOS() << endl;
-
-         t->GetEvent(i);
-         indexfile << r->GetRNAME() << ",";
-         indexfile << i << ",";
-         indexfile << r->GetPOS() << ",";
-            
-      }
-   }
-
-   t->GetEvent(i-1);
-   indexfile << i-1 << ",";
-   indexfile << r->GetPOS() << endl;
-   
-   indexfile.close();
-
-   stopwatch.Print();
 
 }
---
+++ ramstructure.C
@@ -0,0 +1,76 @@
+//
+// Build an index for a RAM file
+//
+// Author: Jose Javier Gonzalez Ortiz, 17/7/2017
+//
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include <TFile.h>
+#include <TTree.h>
+#include <TStopwatch.h>
+
+#include "ramrecord.h"
+
+void ramstructure(const char *file)
+{
+   TStopwatch stopwatch;
+   stopwatch.Start();
+
+   // Create index file
+   ofstream indexfile;
+   std::string indexfilename = file;
+   indexfilename += ".idx";
+   indexfile.open(indexfilename);
+
+   // Open the file and load tree and reader
+   auto f = TFile::Open(file);
+   auto t = (TTree *)f->Get("RAM");
+   RAMRecord *r = 0;
+
+   t->SetBranchAddress("RAMRecord.", &r);
+
+   // Assume RNAME are chunked together
+   // We look only at the RNAME column
+   t->SetBranchStatus("RAMRecord.*", 0);
+   t->SetBranchStatus("RAMRecord.v_rname", 1);
+   t->SetBranchStatus("RAMRecord.v_pos", 1);
+   
+   int i = 0;
+   t->GetEvent(0);
+   indexfile << "RNAME,begin,beginPOS,end,endPOS" << endl;
+   
+   indexfile << r->GetRNAME() << ",";
+   indexfile << i << ",";
+   indexfile << r->GetPOS() << ",";
+   
+   TString current_rname = r->GetRNAME();
+
+   for (i = 0; i < t->GetEntries(); i++) {
+      t->GetEvent(i);
+      if (!current_rname.EqualTo(r->GetRNAME())) {
+         current_rname = r->GetRNAME();
+
+         t->GetEvent(i-1);
+         indexfile << i-1 << ",";
+         indexfile << r->GetPOS() << endl;
+
+         t->GetEvent(i);
+         indexfile << r->GetRNAME() << ",";
+         indexfile << i << ",";
+         indexfile << r->GetPOS() << ",";
+            
+      }
+   }
+
+   t->GetEvent(i-1);
+   indexfile << i-1 << ",";
+   indexfile << r->GetPOS() << endl;
+   
+   indexfile.close();
+
+   stopwatch.Print();
+
+}
