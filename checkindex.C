cfc55e84dd80e7acd64f5d340ca636d86d56067e
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2017-08-07 23:22:41 +0200

    Modify checkindex for new index standard

--- checkindex.C
+++ checkindex.C
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <fstream>
 
 #include <TTree.h>
 #include <TTreeIndex.h>
@@ -14,25 +14,44 @@ void checkindex(const char *file="samexample.root"){
     auto f = TFile::Open(file);
     auto t = (TTree *)f->Get("RAM");
     RAMRecord *r = 0;
-    // t->BuildIndex("v_rname", "v_pos");
     t->SetBranchAddress("RAMRecord.", &r);
-
-    // auto i = TTreeIndex(t, "v_rname", "v_pos");
-
-    t->Print();
 
     auto i  = t->GetTreeIndex();
     if(i){
-        std::cout << "INDEX PRESENT" << std::endl;
-        std::cout << i->GetMajorName() << std::endl;
-        std::cout << i->GetMinorName() << std::endl;
+        std::cout << "Index present in TTree" << std::endl;
     }
     else{
+        i = (TTreeIndex *)f->Get("INDEX");
+        if(i){
+            std::cout << "Index present in same TFile" << std::endl;
+        }
+        else{
+            std::string indexfile = file;
+            indexfile.append(".rai");
+            std::FILE *fp = std::fopen(indexfile.c_str(), "r");
+            if (fp) {
+                auto f2 = TFile::Open(indexfile.c_str());
+                i = (TTreeIndex *)f2->Get("INDEX");
+                if(i){
+                    std::cout << "HOLA" << std::endl;
+                    std::cout << "Index present in separate TFile" << std::endl;
+                }
+                delete fp;
+                delete f2;
+            }
+            
+        }
+    }
+
+    if(i){
+        std::cout << "Majorname is " << i->GetMajorName() << std::endl;
+        std::cout << "Minorname is " << i->GetMinorName() << std::endl;
+    }else{
         std::cout << "INDEX MISSING" << std::endl;
     }
 
-    // i->Print();
-
-    // std::cout << t->GetEntryNumberWithBestIndex(TString::Hash("chr1", std::strlen("chr1")), 10152) << std::endl;
+    delete i;
+    delete t;
+    delete f;
 
 }
