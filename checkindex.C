453c6a749a5c10ed17042a7a4627f4709ab46310
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2017-08-08 11:48:50 +0200

    Change checkindex to ignore TFile cases

--- checkindex.C
+++ checkindex.C
@@ -16,38 +16,35 @@ void checkindex(const char *file="samexample.root"){
     RAMRecord *r = 0;
     t->SetBranchAddress("RAMRecord.", &r);
 
+    // Check for TTreeIndex in TTree, same file and external file (in that order)
+
     auto i  = t->GetTreeIndex();
     if(i){
         std::cout << "Index present in TTree" << std::endl;
     }
     else{
-        i = (TTreeIndex *)f->Get("INDEX");
-        if(i){
-            std::cout << "Index present in same TFile" << std::endl;
-        }
-        else{
-            std::string indexfile = file;
-            indexfile.append(".rai");
-            std::FILE *fp = std::fopen(indexfile.c_str(), "r");
-            if (fp) {
-                auto f2 = TFile::Open(indexfile.c_str());
-                i = (TTreeIndex *)f2->Get("INDEX");
-                if(i){
-                    std::cout << "HOLA" << std::endl;
-                    std::cout << "Index present in separate TFile" << std::endl;
-                }
-                delete fp;
-                delete f2;
+
+        std::string indexfile = file;
+        indexfile.append(".rai");
+        std::FILE *fp = std::fopen(indexfile.c_str(), "r");
+        if (fp) {
+            auto f2 = TFile::Open(indexfile.c_str());
+            i = (TTreeIndex *)f2->Get("INDEX");
+            if(i){
+                std::cout << "HOLA" << std::endl;
+                std::cout << "Index present in separate TFile" << std::endl;
             }
-            
+            delete fp;
+            delete f2;
         }
+
     }
 
     if(i){
         std::cout << "Majorname is " << i->GetMajorName() << std::endl;
         std::cout << "Minorname is " << i->GetMinorName() << std::endl;
     }else{
-        std::cout << "INDEX MISSING" << std::endl;
+        std::cout << "Index Missing" << std::endl;
     }
 
     delete i;
